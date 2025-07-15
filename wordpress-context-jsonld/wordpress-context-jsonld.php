9f0a900ec7e1ba2067a3427ecaee5d31a2e8d365
Maxime Lecoq-Gaillard <maxime@lecoqlibre.fr>
2025-07-15 15:39:54 +0200

    Release 1.1.0 using context v1.16

--- wordpress-context-jsonld/wordpress-context-jsonld.php
+++ wordpress-context-jsonld/wordpress-context-jsonld.php
@@ -3,7 +3,7 @@
  * Plugin Name:       WordPress Context Jsonld
  * Plugin URI:        https://github.com/datafoodconsortium/wordpress-context-jsonld
  * Description:       Adds an alternate link HTTP header to provide a JSON-LD context.
- * Version:           1.0.0
+ * Version:           1.1.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Maxime Lecoq
@@ -18,7 +18,7 @@
 add_filter('wp_headers', function ($headers, $wp) {
 	if ($_SERVER["REQUEST_URI"] === '/') {
 		$dir = "/wp-content/plugins/wordpress-context-jsonld/";
-		$fileName = "context.jsonld";
+		$fileName = "context_1.16.0.jsonld";
 		$context = $dir . $fileName;
 		$headers['access-control-allow-origin'] = "*";
 		$headers['access-control-expose-headers'] = "Link";
